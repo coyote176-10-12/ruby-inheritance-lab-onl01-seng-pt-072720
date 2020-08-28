1f0aba43088e8335fd1569d0854776cd3c26351f
Javier Aguilar <coyote176@msn.com>
2020-08-28 18:56:05 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -2,16 +2,6 @@ class User
   
   attr_accessor :first_name, :last_name
   
- # @@all= []
-  
-    #def initialize(first_name, last_name)
-   ## @first_name= first_name
-     # @last_name= last_name
-      #@@all<< self
-   # end
-    
-   # def self.all
-     # @@all
-   # end
-
 end
+
+
