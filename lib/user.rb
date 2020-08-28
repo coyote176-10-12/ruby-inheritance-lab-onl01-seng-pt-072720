8864b440128754c17f8e8c638b4def80534626d1
Javier Aguilar <coyote176@msn.com>
2020-08-28 19:00:08 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -5,29 +5,3 @@ class User
 end
 
 
-class Teacher < User 
-
-  KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
-  
-  def teach 
-    KNOWLEDGE.sample
-  end
- 
-  
-end
-
-
-class Student < User 
-
- def initialize()
-   @knowledge= []
- end
- 
- def learn(knowledge)
-   @knowledge<< knowledge
-  end
- 
- def knowledge
-   @knowledge
- end
-end
